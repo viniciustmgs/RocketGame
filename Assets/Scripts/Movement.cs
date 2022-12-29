096c037f274f6c98ad9b81cf9ed434eddb6af318
viniciustmgs <viniciustmgs@gmail.com>
2022-12-29 17:17:06 -0300

    Initial Commit

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,64 @@
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    Rigidbody rocketRigidBody;
+    Transform rocketTransform;
+    public AudioSource rocketThrustSound, rocketRotationSound;
+    float rocketThrust = 750f, rocketRotation = 150f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rocketRigidBody = GetComponent<Rigidbody>();
+        rocketTransform = GetComponent<Transform>();
+        rocketThrustSound = GameObject.Find("ThrustSound").GetComponentInChildren<AudioSource>();
+        rocketRotationSound = GameObject.Find("RotationSound").GetComponentInChildren<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessInput();
+    }
+
+    void ProcessInput(){
+        
+        //manage the thrust
+        if(Input.GetKey(KeyCode.W)){
+            rocketRigidBody.AddRelativeForce(Vector3.up * rocketThrust * Time.deltaTime);
+            if(rocketThrustSound.volume == 0){
+                rocketThrustSound.volume = 1;
+            }
+        }
+        else if(rocketThrustSound.volume == 1){
+            rocketThrustSound.volume = 0;
+        }
+    
+        //manage the rotation
+        if(Input.GetKey(KeyCode.A) && !Input.GetKey(KeyCode.D))
+        {
+            Rotate(rocketRotation);
+            if(rocketRotationSound.volume == 0){
+                rocketRotationSound.volume = 0.5f;
+            }
+        }
+        else if(Input.GetKey(KeyCode.D) && !Input.GetKey(KeyCode.A)){
+            Rotate(-rocketRotation);
+            if(rocketRotationSound.volume == 0){
+                rocketRotationSound.volume = 0.5f;
+            }
+        }
+        else{
+            if(rocketRotationSound.volume > 0){
+                rocketRotationSound.volume = 0;
+            }
+        }
+    }
+
+    void Rotate(float rotationPerFrame)
+    {   
+        rocketRigidBody.freezeRotation = true;
+        rocketTransform.Rotate(Vector3.forward * rotationPerFrame * Time.deltaTime);
+        rocketRigidBody.freezeRotation = false;
+    }
+}
